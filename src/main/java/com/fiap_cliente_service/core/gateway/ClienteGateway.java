0e90ef1dec6a042747020972edd4cbc6020f4474
João Victor Fernandes Mendes <joaovictorf01@gmail.com>
2025-06-19 21:43:43 -0300

    ajuste estrutura do projeto.

--- src/main/java/com/fiap_cliente_service/core/gateway/ClienteGateway.java
+++
@@ -1,9 +0,0 @@
-package com.fiap_cliente_service.core.gateway;
-
-import com.fiap_cliente_service.core.domain.Cliente;
-
-public interface ClienteGateway {
-
-    Long criar(Cliente cliente);
-
-}
--- src/main/java/com/fiap_cliente_service/core/usecase/CriarClienteUsecase.java
+++
@@ -1,9 +0,0 @@
-package com.fiap_cliente_service.core.usecase;
-
-import com.fiap_cliente_service.core.domain.Cliente;
-
-public interface CriarClienteUsecase {
-
-    Long criar(Cliente cliente);
-
-}
