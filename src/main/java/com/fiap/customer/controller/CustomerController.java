d328e8eecb01a16a90003b4a09495f956dad6c36
João Victor Fernandes Mendes <joaovictorf01@gmail.com>
2025-07-08 15:27:57 -0300

    ajustando documentação

--- src/main/java/com/fiap/customer/controller/CustomerController.java
+++ src/main/java/com/fiap/customer/controller/CustomerController.java
@@ -6,12 +6,17 @@ import com.fiap.customer.controller.response.CustomerResponse;
 import com.fiap.customer.domain.Customer;
 import com.fiap.customer.usecase.CreateCustomerUseCase;
 import com.fiap.customer.usecase.GetCustomerByIdUseCase;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/customers")
+@Tag(name = "Customers", description = "API para gerenciamento de clientes")
 public class CustomerController {
 
     private final CreateCustomerUseCase createCustomerUseCase;
@@ -28,6 +33,7 @@ public class CustomerController {
     }
 
     @PostMapping
+    @Operation(summary = "Criar novo cliente", description = "Cria um novo cliente com endereço e dados pessoais")
     public ResponseEntity<CustomerResponse> create(@RequestBody CustomerRequest request) {
         Customer customer = customerMapper.toDomain(request);
         Long id = createCustomerUseCase.execute(customer);
@@ -36,6 +42,8 @@ public class CustomerController {
     }
 
     @GetMapping("/{id}")
+
+    @Operation(summary = "Buscar cliente por ID", description = "Retorna os dados de um cliente existente")
     public ResponseEntity<CustomerResponse> findById(@PathVariable Long id) {
         Customer customer = getCustomerByIdUseCase.execute(id);
         CustomerResponse response = customerMapper.toResponse(customer, customer.getId());
